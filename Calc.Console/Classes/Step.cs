c8a2db90564a64c208595b5bc730f018235014b2
nnevsky <nnevsky@gmail.com>
2021-11-23 10:43:44 +0300

    Update Calc.Console.sln, Calc.Console.csproj, and 3 more files...

---
+++ Calc.Console/Classes/Step.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calc.Console.Classes
+{
+    interface IStep
+    {
+        IStep NextStep { get; }
+
+        double Result { get; }
+    }
+
+    abstract class Step : IStep
+    {
+        protected string line { get; }
+
+        public Step(string line)
+        {
+            this.line = line;
+        }
+        public abstract IStep NextStep { get; }
+
+        public abstract double Result { get; }
+    }
+
+}
