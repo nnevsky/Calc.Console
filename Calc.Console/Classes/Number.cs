a585034ed3f4a51dc20aa144a64e6684e21cb786
nnevsky <nnevsky@gmail.com>
2021-11-23 18:55:02 +0300

    Update Number.cs and Step.cs

--- Calc.Console/Classes/Number.cs
+++ Calc.Console/Classes/Number.cs
@@ -4,7 +4,90 @@ using System.Text;
 
 namespace Calc.Console.Classes
 {
-    class Number
+    class Number : Step
     {
+        private readonly double? result;
+        public Number(string line) : base(line)
+        {
+            if(double.TryParse(line, out double res))
+            {
+                result = res;
+            }
+            else
+            {
+                ParseString();
+            }
+        }
+
+       
+        public override IStep NextStep { get; }
+
+        public override double Result => result ?? NextStep.Result;
     }
+
+    abstract class Operation : Step
+    {
+        protected Operation(string line, char op) : base(line)
+        {
+           
+        }
+
+        public static IStep ParseOperation(string line)
+        {
+            foreach (char c in line)
+            {
+                switch(c)
+                {
+                    case '+': return new Plus(line);
+                    case '-': return new Minus(line);
+                    case '*': return new Multiple(line);
+                    case '/': return new Divide(line);
+                    default: return null;
+                }
+            }
+            return null;
+        }
+    }
+
+    class Plus : Operation
+    {
+        public Plus(string line) : base(line, '+')
+        {
+
+        }
+        public override IStep NextStep => throw new NotImplementedException();
+
+        public override double Result => throw new NotImplementedException();
+    }
+    class Minus : Operation
+    {
+        public Minus(string line) : base(line, '-')
+        {
+
+        }
+        public override IStep NextStep => throw new NotImplementedException();
+
+        public override double Result => throw new NotImplementedException();
+    }
+    class Multiple : Operation
+    {
+        public Multiple(string line) : base(line, '*')
+        {
+
+        }
+        public override IStep NextStep => throw new NotImplementedException();
+
+        public override double Result => throw new NotImplementedException();
+    }
+    class Divide : Operation
+    {
+        public Divide(string line) : base(line, '/')
+        {
+
+        }
+        public override IStep NextStep => throw new NotImplementedException();
+
+        public override double Result => throw new NotImplementedException();
+    }
+
 }
--- Calc.Console/Classes/Step.cs
+++ Calc.Console/Classes/Step.cs
@@ -11,6 +11,9 @@ namespace Calc.Console.Classes
         double Result { get; }
     }
 
+    /// <summary>
+    /// Шаг выполнения вычислений
+    /// </summary>
     abstract class Step : IStep
     {
         protected string line { get; }
@@ -22,6 +25,28 @@ namespace Calc.Console.Classes
         public abstract IStep NextStep { get; }
 
         public abstract double Result { get; }
+
+        protected string ParseString()
+        {
+            foreach (char c in line)
+            {
+                int openBreaker = line.IndexOfAny("({[".ToCharArray());
+                if (openBreaker != -1) // Если есть открывающая скобка
+                {
+                    int closeBreaker = line.IndexOfAny("]})".ToCharArray());
+                    if (closeBreaker != -1) // Если есть закрывающая скобка
+                    {
+                        int secondBreaker = line.IndexOfAny("({[".ToCharArray(), openBreaker + 1);
+                        if (secondBreaker != -1) // Если между ними есть вложенная открывающая скобка
+                        {
+
+                        }
+                    }
+                    else throw new InvalidOperationException($"Нет закрывающей скобки в строке '{line}'");
+                }
+            }
+            return line; // В строке нет скобок - вернуть как есть
+        }
     }
 
 }
